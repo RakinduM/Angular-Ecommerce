39b0b73b4f119ed50dea35aea75eceeff9c7b582
Rakindu Marambe <116582517+RakinduM@users.noreply.github.com>
2025-01-10 13:29:40 +0530

    Add initial implementation of header and primary button components with Tailwind CSS integration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [RouterOutlet, HeaderComponent],
+  template: `
+    <app-header/>
+    <router-outlet />
+  `,
+  styles: [],
 })
-export class AppComponent {
-  title = 'Angular-Ecommerce';
-}
+export class AppComponent {}
